04559d30a93e9e337c46e24ee91c1c27ef2d5aaa
makerj <ohenwkgdj@gmail.com>
2015-08-16 04:09:43 +0900

    First impl. not tested

--- AndroidDecompiler.bat
+++ AndroidDecompiler.bat
@@ -1,5 +1,4 @@
 @echo off
-
 echo **************************************************
 echo ApkDDD "Simplest Apk decompiler"
 echo Copyright (c) 2015 Junhui Lee(ohenwkgdj@gmail.com)
@@ -10,7 +9,7 @@ REM --SET VARIABLE -----------------------
 SET _root=%CD%
 SET _7zout=%_root%\_7zoutPath
 SET _dexout=%_root%\_dexoutPath
-SET _childName=AD_Child
+SET _CHILD_PROCESS_NAME=AD_Child
 
 SET _returnPos=0
 SET _arg0=0
@@ -28,7 +27,7 @@ IF NOT EXIST "%_7zout%" mkdir %_7zout%
 IF NOT EXIST "%_dexout%" mkdir %_dexout%
 
 REM --Validate variables -----------------
-IF [%0]==[] (
+IF [%1]==[] (
    GOTO ERROR
 )
 
@@ -36,16 +35,16 @@ REM --Start Commands ---------------------
 
    REM --Step1: extract apk---------------
       echo Extracting apk archive...
-      echo %_7z% x -y -o"%_7zout%" %1
+      %_7z% x -y -o"%_7zout%" %1
       echo.
 
    REM --Step2: convert dex to jar--------
       echo Converting dex to jar...
-      CD %_dexout%
-      START "%_childName%" %_dex2jar% --force %_7zout%\classes.dex
-   
+      CD %_dexout%   
       SET _returnPos=STEP2_DONE
       SET _arg0=%_dexout%\classes-dex2jar.jar
+      SET _arg1=STEP2
+      START /B "%_CHILD_PROCESS_NAME%%_arg1%" /B %_dex2jar% --force %_7zout%\classes.dex
       GOTO HANDLE_OUTPUT
       :STEP2_DONE
    
@@ -54,18 +53,25 @@ REM --Start Commands ---------------------
 
    REM --Step3: decompile jar to java classes----
       echo Decompiling jar to java...
-      START "%_childName%" %_jd% %_dexout%\classes-dex2jar.jar
-      
       SET _returnPos=STEP3_DONE
       SET _arg0=%_root%\classes-dex2jar.src.jar
+      SET _arg1=STEP3
+      START "%_CHILD_PROCESS_NAME%%_arg1%" /B %_jd% %_dexout%\classes-dex2jar.jar
       GOTO HANDLE_OUTPUT
       :STEP3_DONE
-   
       REN %_root%\classes-dex2jar.src.jar done.zip
       echo.
 
+REM AFTER CLEANING------------------------
+RMDIR /S /Q %_7zout%
+RMDIR /S /Q %_dexout%
+DEL /Q *.src.jar
+DEL /Q null
+CLS
+
 REM --Exit -------------------------------
-SET done="DONE"
+echo Your apk Successfully decompiled seeya!
+EXIT
 
 
 
@@ -73,14 +79,19 @@ SET done="DONE"
 
 
 
-
-
-
+REM --------------------------------HANDLE_OUTPUT
 :HANDLE_OUTPUT
-TIMEOUT 1 >nul
-IF NOT EXIST "%_arg0" GOTO HANDLE_OUTPUT
-TASKKILL /FI "WINDOWTITLE EQ %_childName%"
-GOTO %_returnPos%
+   SET /a __tryCount=0
+   SET /a _FIVE=5
+   :__HANDLE_OUTPUT
+      SET /a __tryCount+=1
+      TIMEOUT 5 >nul
+   REM TRY OUT--------------------------------------
+      IF %__tryCount% EQU %_FIVE% GOTO ERROR
+   REM FILE EXIST CHECK LOOP------------------------
+      IF NOT EXIST %_arg0% GOTO __HANDLE_OUTPUT
+   GOTO %_returnPos%
+REM --------------------------------HANDLE_OUTPUT
 
 
 
@@ -91,6 +102,5 @@ GOTO %_returnPos%
 
 
 
-
-
-
+:ERROR
+echo Welcome to ERORR world
